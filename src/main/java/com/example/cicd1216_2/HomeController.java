d4d6f9e78c677dcfa5cba0b3b1318da9318365a5
SMY7527 <kdo7527@naver.com>
2025-12-16 11:36:48 +0900

    test

--- src/main/java/com/example/cicd1216_2/HomeController.java
+++ src/main/java/com/example/cicd1216_2/HomeController.java
@@ -7,16 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/hicicd")
     public String hicicd() {
-        return "hi, CICD 1216^^";
+        return "hi, CICD2 1216^^";
     }
 
     @GetMapping("/hicicd2")
     public String hicicd2() {
-        return "hi2, New CICD 1216^^";
+        return "hi2, New CICD2 1216^^";
     }
 
     @GetMapping("/hicicd3")
     public String hicicd3() {
-        return "hi3, New CICD 적용 1216^^";
+        return "hi3, New CICD2 적용 1216^^";
     }
 }
